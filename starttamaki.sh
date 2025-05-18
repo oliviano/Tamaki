66e40ee3ab5ab33634828e6c0cce1dc63daea808
Olivier Jean <oliviano@gmail.com>
2025-05-18 18:06:45 +1200

    Create starttamaki.sh
    Added Start Script. Mk3 :)

---
+++ starttamaki.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This will run the Python script in the virtualenv, inside the 'tamaki' screen
+# This assume sudoers has been modified.
+screen -S tamaki -dm bash -c "source env/bin/activate && python3 tamaki_udp_sender.py"
